3f80418843a05478c5ca40a90d6f50b35b1093f1
Sastre <sastrd@metmuseum.org>
2021-02-07 22:54:13 -0500

    First html asigment

---
+++ tutorial1_getting_started/main.js
@@ -0,0 +1,16 @@
+console.log("Ciao mondo")
+
+
+function convert(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
+
+var temperature=32;
+
+function x(number){
+
+  return (number/8);
+}
+
+
+console.log("Today, New York City registrated" + " " +convert(temperature) + "° Celsius,"  + " " + x(temperature) + "°"+ "colder than yesterday");
